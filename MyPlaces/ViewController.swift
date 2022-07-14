4fdd7570c0060b2860d914db156664120805204a
MrCronkite <vladshima99@gmail.com>
2022-07-14 22:46:28 +0300

    uitablevie

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    
+    let places = ["ШОК","Классик", "Балкан Гриль", "Токини", "Спички", "Васельки","Мир" ,"ЛидБир"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        cell?.textLabel?.text = "Cell"
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = places[indexPath.row]
+        cell.imageView?.image = UIImage(named: "cell_img")
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 78
     }
 
 
