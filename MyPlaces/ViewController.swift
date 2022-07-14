eb12d501d72c36c30a91dd4b58f251648791c0b3
MrCronkite <vladshima99@gmail.com>
2022-07-14 21:07:25 +0300

    first commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by admin1 on 14.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+
+
+}
+
