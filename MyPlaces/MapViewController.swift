29bbca4ecbbf76e68e52d37e18596e9823fc0872
MrCronkite <vladshima99@gmail.com>
2022-07-27 21:01:36 +0300

    map anotation

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -15,11 +15,40 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPlacemark()
 
     }
     
     @IBAction func closeVC(_ sender: Any) {
         dismiss(animated: true)
+    }
+    
+    private func setupPlacemark() {
+        guard let loccation = place.location else {return}
+        
+        //конвертирует адрес
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(loccation) {(placemarks, error) in
+            if let error = error{
+                print(error)
+                return
+            }
+            guard let placemarks = placemarks else {return}
+            
+            let placemark = placemarks.first
+            
+            //описание точки на карте
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else {return}
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
     }
     
 
