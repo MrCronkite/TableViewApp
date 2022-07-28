0303f34a811366162476d60d9f2c93760c484e1b
MrCronkite <vladshima99@gmail.com>
2022-07-28 08:15:38 +0300

    bag fix

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -62,11 +62,12 @@ class NewPlaceViewController: UITableViewController {
     //MARK: Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier != "showMap" {
-            return
-        }
+        if segue.identifier != "showMap" { return }
         let mapVC = segue.destination as! MapViewController
-        mapVC.place = currentPlace
+        mapVC.place.name = placeName.text!
+        mapVC.place.location = placeLocation.text
+        mapVC.place.type = placeType.text
+        mapVC.place.imageData = placeImage.image?.pngData()
     }
     
     
@@ -74,13 +75,7 @@ class NewPlaceViewController: UITableViewController {
     
     func savePlace() {
         
-        var image: UIImage?
-        
-        if imageIsChanged {
-            image = placeImage.image
-        } else {
-            image = #imageLiteral(resourceName: "cell_img")
-        }
+        let image = imageIsChanged ? placeImage.image : #imageLiteral(resourceName: "cell_img")
         
         let imageData = image?.pngData()
     
